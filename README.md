4fd765f9a1bd2ffeff8a8a5ab9b4e558b1050bf0
sand <shmsand@gmail.com>
2018-11-27 22:02:19 -0800

    Fix

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dd-phase2-template
+在 assets/js/vis01.js 里面改
+改 visualization04.html 里面的东西
--- _site/assets/js/vis01.js
+++ _site/assets/js/vis01.js
@@ -1,14 +1,12 @@
-$(document).ready(function() {
-    $("[id = rectfirst]").hover(function(event) {
-      let index = $(event.currentTarget).index() + 1;
-      $("[id = visflabel]")
-        .removeClass("visfirst05");
-      console.log(index);
-      // change this part tot find the text related to the rectbox
-      $("#visflabel")
-        .children(":nth-child(" + index + ")").addClass("visfirst05");
-      // console.log(  $("#visflabel")
-      // .children(":nth-child(" + index + ")")
-      // .find("text"))
-    });
+$(document).ready(function () {
+  $("[id = rectfirst]").hover(function(event) {
+    let index = $(event.currentTarget).index() + 1;
+    $(".visfirst05").removeClass("visfirst05");
+
+    // change this part tot find the text related to the rectbox
+    console.log(index);
+    $("#visflabel")
+      .children(":nth-child(" + index + ")")
+      .addClass("visfirst05");
+  });
 });
