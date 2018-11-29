012a55bf742805506011022218e582a214f57242
Bao YiFang <byf.aisling@gmail.com>
2018-11-29 02:17:15 +0100

    a

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,41 +1,62 @@
-// This is Javascript code to make the example in question02 work, fell free to remove this when you start working on your project
-$(document).ready(function() {
+$(document).ready(function () {
+    $("[id = rectfirst]").hover(function(event) {
+      let index = $(event.currentTarget).index() + 1;
+      $(".clslabela").removeClass("clslabela");
 
-    $('.bubble').on('click', function(event) {
-
-        event.stopPropagation();
-        let listIndex = $(event.currentTarget).index() + 1;
-
-        $('.label').removeClass('is-visible');
-        $('.label:nth-child(' + listIndex + ')').addClass('is-visible');
-
+      // change this part tot find the text related to the rectbox
+      console.log(index);
+      $("#visflabel")
+        .children(":nth-child(" + index + ")")
+        .addClass("clslabela");
     });
+  });
+  
 
-    $(':not(.bubble)').on('click', function(event) {
 
-        // console.log('event fired!');
-        $('.label').removeClass('is-visible');
 
-    })
 
-    $('.legend__item').on('mouseenter', function(event) {
 
-        // console.log($(event.currentTarget).attr("data-area"));
-        let area = $(event.currentTarget).attr("data-area");
 
-        $('.label').removeClass('is-visible');
 
-        $('.legend__item').addClass('is-out-focus');
-        $(event.currentTarget).removeClass('is-out-focus');
 
-        $('.bubble').addClass('is-out-focus');
-        $('.' + area).removeClass('is-out-focus');
+// This is Javascript code to make the example in question02 work, fell free to remove this when you start working on your project
+// $(document).ready(function() {
 
-    }).on('mouseleave', function(event) {
+//     $('.bubble').on('click', function(event) {
 
-        $('.bubble').removeClass('is-out-focus');
-        $('.legend__item').removeClass('is-out-focus');
+//         event.stopPropagation();
+//         let listIndex = $(event.currentTarget).index() + 1;
 
-    })
+//         $('.label').removeClass('is-visible');
+//         $('.label:nth-child(' + listIndex + ')').addClass('is-visible');
 
-});
+//     });
+
+//     $(':not(.bubble)').on('click', function(event) {
+
+//         // console.log('event fired!');
+//         $('.label').removeClass('is-visible');
+
+//     })
+
+//     $('.legend__item').on('mouseenter', function(event) {
+
+//         // console.log($(event.currentTarget).attr("data-area"));
+//         let area = $(event.currentTarget).attr("data-area");
+
+//         $('.label').removeClass('is-visible');
+
+//         $('.legend__item').addClass('is-out-focus');
+//         $(event.currentTarget).removeClass('is-out-focus');
+
+//         $('.bubble').addClass('is-out-focus');
+//         $('.' + area).removeClass('is-out-focus');
+
+//     }).on('mouseleave', function(event) {
+
+//         $('.bubble').removeClass('is-out-focus');
+//         $('.legend__item').removeClass('is-out-focus');
+
+//     })
+
+// });
