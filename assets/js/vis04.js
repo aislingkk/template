a3c43961548c44cc8d87f507b8f243524c68af6b
sand <shmsand@gmail.com>
2018-11-28 20:05:25 -0800

    Fix

--- assets/js/vis04.js
+++ assets/js/vis04.js
@@ -1,12 +1,11 @@
-// $(document).ready(function () {
-//   $("[id = rectfirst]").hover(function(event) {
-//     let index = $(event.currentTarget).index() + 1;
-//     $(".clslabela").removeClass("clslabela");
+$(document).ready(function () {
+  $("[id = redcircle]").hover(function(event) {
+    let index = $(event.currentTarget).index() + 1;
+    $(".event-show").removeClass("event-show");
 
-//     // change this part tot find the text related to the rectbox
-//     console.log(index);
-//     $("#visflabel")
-//       .children(":nth-child(" + index + ")")
-//       .addClass("clslabela");
-//   });
-// });
+    // change this part tot find the text related to the rectbox
+    $("#eventtext")
+      .children(":nth-child(" + index + ")")
+      .addClass("event-show");
+  });
+});
